37429eb5f78c11276dc4d20d3880d34157055ce9
CassandreV <cassandre.vanzetta@live.fr>
2020-06-27 16:10:42 +0200

    iteration 1 step 4 done

--- js/index.js
+++ js/index.js
@@ -2,18 +2,13 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-  window.addEventListener('load', () => {
-    const calculatePricesBtn = document.getElementById('calculate');
-    calculatePricesBtn.onclick= function(){
-      alert('Calculate Prices clicked!')}
-    calculatePricesBtn.addEventListener('click', calculateAll);
-    const price = product.querySelector('.price span');
-    const quantity = product.querySelector('.quantity span');
 
-  });
-
-  //... your code goes here
+    const price = product.querySelector('.price span').innerHTML;
+    const quantity = product.querySelector('.quantity input').value;
+    const total = price * quantity;
+    product.querySelector('.subtotal span').innerHTML = total;
 }
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
